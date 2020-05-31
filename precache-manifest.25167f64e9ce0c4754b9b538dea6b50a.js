511b421e7e081e02e2f0aae7d5c1be36bd6ea082
patryk-kilian <patryk.kilian93@gmail.com>
2020-05-31 13:25:23 +0200

    Updates

---
+++ precache-manifest.25167f64e9ce0c4754b9b538dea6b50a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f246d5fb9e156597293b189bf263b895",
+    "url": "/starship-store/index.html"
+  },
+  {
+    "revision": "41f14c76746b1e91ca89",
+    "url": "/starship-store/static/js/2.1c3b036f.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/starship-store/static/js/2.1c3b036f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aac8d3454c91ffc8c4d",
+    "url": "/starship-store/static/js/main.aee7a631.chunk.js"
+  },
+  {
+    "revision": "059ef3897e4a03cccb92",
+    "url": "/starship-store/static/js/runtime-main.16a3490d.js"
+  }
+]);
